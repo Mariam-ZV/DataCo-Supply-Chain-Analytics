71646d919750ddfa2d2e267a19705cb4839d0cf1
Mariam Zamanvaziri <117606811+Mariam-ZV@users.noreply.github.com>
2026-06-13 22:22:30 +0100

    Update 03_4_dim_department.sql

--- sql/03_dimensions/03_4_dim_department.sql
+++ sql/03_dimensions/03_4_dim_department.sql
@@ -5,3 +5,23 @@ CREATE TABLE dim_department (
 );
 
 SELECT * FROM dim_department;
+-------------------------------------------------------------
+
+INSERT INTO dim_department (
+    department_id,
+    department_name
+)
+SELECT DISTINCT
+    `Department Id`,
+    `Department Name`
+FROM dataco_raw;INSERT INTO dim_department (
+    department_id,
+    department_name
+)
+SELECT DISTINCT
+    `Department Id`,
+    `Department Name`
+FROM dataco_raw;
+-------------------------------------------------------------
+
+SELECT COUNT(*) FROM dim_department;
