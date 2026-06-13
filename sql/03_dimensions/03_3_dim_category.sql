aa3d8feb5dc2ddbb31e37c2ab9ac9e3b82f7969f
Mariam Zamanvaziri <117606811+Mariam-ZV@users.noreply.github.com>
2026-06-13 22:13:19 +0100

    Update 03_3_dim_category.sql

--- sql/03_dimensions/03_3_dim_category.sql
+++ sql/03_dimensions/03_3_dim_category.sql
@@ -6,3 +6,18 @@ CREATE TABLE dim_category (
 );
 
 SELECT * FROM dim_category;
+-----------------------------------------------------------
+
+INSERT INTO dim_category (
+    category_id,
+    category_name,
+    department_id
+)
+SELECT DISTINCT
+    `Category Id`,
+    `Category Name`,
+    `Department Id`
+FROM dataco_raw;
+-----------------------------------------------------------
+
+SELECT COUNT(*) FROM dim_category;
